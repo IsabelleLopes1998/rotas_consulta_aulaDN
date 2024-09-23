5a3a68053265a3a2af4dc2a426ff4ad0f9b1992e
IsabelleLopes <isabelle.biomed@gmail.com>
2024-09-23 10:16:54 -0300

    add porta'/produto' no diretorio localhost:4000

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req,res){
-    res.send("Bem vindo ao meu site")
+    res.send("Bem vindo ao meu site. Meu nome é Isabelle, me dê um real")
 })
 
+// add rota de produto
+app.get("/produtos", function(req,res){
+    res.send("<h1>Lista de Produtos!<h1>")
+})
 
 app.listen(4000,function(erro){
     if(erro){

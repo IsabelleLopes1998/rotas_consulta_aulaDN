5c8669446d08aea3b76e8a6fa4a022a2926e0ad6
IsabelleLopes <isabelle.biomed@gmail.com>
2024-09-23 10:40:54 -0300

    sei lá

---
+++ Calculadora.js
@@ -0,0 +1,27 @@
+function soma (a,b){
+    return a + b;
+}
+function multiplicacao(a,b){
+    return a * b;
+}
+
+function divisao (a,b){
+    return a / b; 
+}
+function subtracao(a,b){
+    return a - b
+}
+
+console.log(soma(2,5));
+
+var temp = multiplicacao(2,5)
+var temp2 = divisao(30,2);
+
+console.log(subtracao(temp2,temp))
+
+module.exports ={
+    soma,
+    multiplicacao,
+    divisao,
+    subtracao
+}
